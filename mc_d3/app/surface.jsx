1614ac3d052e53241204264cadb379d5d50bc15d
Sam <s.battenally@gmail.com>
2018-09-08 17:19:26 +1000

    comment out issue

--- mc_d3/app/surface.jsx
+++ mc_d3/app/surface.jsx
@@ -11,36 +11,36 @@ class Surface extends React.Component {
     // Get the DOMElement that would be used to insert the chart
     var el = ReactDOM.findDOMNode(this).getElementsByClassName('chart3')[0];
 
-    Plotly.d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/api_docs/mt_bruno_elevation.csv', function(err, rows){
-        function unpack(rows, key) {
-          return rows.map(function(row) { return row[key]; });
-        }
+    // Plotly.d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/api_docs/mt_bruno_elevation.csv', function(err, rows){
+    //     function unpack(rows, key) {
+    //       return rows.map(function(row) { return row[key]; });
+    //     }
           
-        var z_data=[ ]
-        for(let i=0;i<24;i++)
-        {
-          z_data.push(unpack(rows,i));
-        }
+    //     var z_data=[ ]
+    //     for(let i=0;i<24;i++)
+    //     {
+    //       z_data.push(unpack(rows,i));
+    //     }
         
-        var data = [{
-                   z: z_data,
-                   type: 'surface'
-                }];
+    //     var data = [{
+    //                z: z_data,
+    //                type: 'surface'
+    //             }];
           
-        var layout = {
-          title: 'Mt Bruno Elevation',
-          autosize: false,
-          width: 500,
-          height: 500,
-          margin: {
-            l: 65,
-            r: 50,
-            b: 65,
-            t: 90,
-          }
-        };
-        Plotly.newPlot(el, data, layout);
-        });
+    //     var layout = {
+    //       title: 'Mt Bruno Elevation',
+    //       autosize: false,
+    //       width: 500,
+    //       height: 500,
+    //       margin: {
+    //         l: 65,
+    //         r: 50,
+    //         b: 65,
+    //         t: 90,
+    //       }
+    //     };
+    //     Plotly.newPlot(el, data, layout);
+    //     });
 
   }
 
