effdce4a94bdc7dfed85c0d11882ee63a9f40967
Alexander Spence <alexander@diversityarrays.com>
2018-09-08 15:08:28 +1000

    Changes to dependencies to try to get react-d3 sanky to work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import {ForceGraph2D} from 'react-force-graph';
+// import {ForceGraph2D} from 'react-force-graph';
 import Header from './components/Header'
+import SankeyChart from './SankeyChart';
+import { ExportJSON, ImportJSON, loadData, readFile } from './utils';
+
 
 class App extends Component {
-  state = {
-    data : null,
-  };
+
+  constructor() {
+    super()
+
+    this.state = {
+      nodes: [],
+      links: [],
+    };
+
+    this.loadData = loadData.bind(this);
+  }
 
   componentDidMount() {
-    this._request = fetch('/api/testdata')
-    .then(res => res.json())
-    .then(data => {
-      this.setState({data});
-      this.render();
-    }).catch(error => {
-      console.log(error);
-    });
-    console.log("Trying to load your data!");
+    this.loadData('http://localhost:5000/api/data');//../server/datasets/data.json');
+
+    // this._request = fetch('/api/nodes')
+    // .then(res => res.json())
+    // .then(nodes => {
+    //   this.setState({nodes});
+    // }).catch(error => {
+    //   console.log(error);
+    // });
+    //
+    // this._request = fetch('/api/links')
+    // .then(res => res.json())
+    // .then(links => {
+    //   this.setState({links});
+    //   this.render();
+    // }).catch(error => {
+    //   console.log(error);
+    // });
+    // console.log("Trying to load your data!");
+
   }
 
   componentWillUnmount() {
@@ -31,9 +53,9 @@ class App extends Component {
     console.log("Render called.");
     const heading = "WarmCall";
 
-    console.log("Data -> " + this.state.data );
+    console.log("Data -> " + this.state.nodes);
 
-    if (this.state.data == null) {
+    if (this.state.nodes == null && this.state.links) {
       return (
         <div className="App">
           <header className="App-header">
@@ -48,14 +70,7 @@ class App extends Component {
       return (
         <div className="App">
             <Header/>
-        <ForceGraph2D
-        graphData={nodes}
-        nodeLabel="id"
-        nodeAutoColorBy="group"
-        linkDirectionalParticles="value"
-        nodeVal={n => n.size}
-        linkDirectionalParticleSpeed={d => d.value * 0.001}
-      />
+            <SankeyChart nodes={this.state.nodes} links={this.state.links} openModal={this.openModal}/>
               </div>
       );
     }
