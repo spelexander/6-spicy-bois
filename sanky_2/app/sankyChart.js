fbbc13b57ec967c481909b3488b51fab021dd66c
Alexander Spence <alexander@diversityarrays.com>
2018-09-08 20:43:52 +1000

    i missed this

---
+++ sanky_2/app/sankyChart.js
@@ -0,0 +1,30 @@
+
+// Reads sanky data
+var json = JSON.parse(data);
+consol.console.log(data);
+
+var configSankey = {
+    margin: { top: 10, left: 10, right: 10, bottom: 10 },
+    nodes: {
+      dynamicSizeFontNode: {
+        enabled: true,
+        minSize: 14,
+        maxSize: 30
+      },
+      draggableX: false, // default [ false ]
+      draggableY: true, // default [ true ]
+      colors: d3.scaleOrdinal(d3.schemeCategory10)
+    },
+    links: {
+      formatValue: function(val) {
+        return d3.format(",.0f")(val) + ' TWh';
+      }
+    },
+    tooltip: {
+      infoDiv: true,
+      labelSource: 'Input:',
+      labelTarget: 'Output:'
+    }
+}
+
+var objSankey = sk.createSankey('#chart1', configSankey, json);
