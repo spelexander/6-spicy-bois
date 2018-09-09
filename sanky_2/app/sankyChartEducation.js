984914c063f0c493f5a18e15f217d2b94504e0af
Michael <michael@Michaels-MBP.lan>
2018-09-09 12:38:37 +1000

    education still not working

--- sanky_2/app/sankyChartEducation.js
+++ sanky_2/app/sankyChartEducation.js
@@ -1,7 +1,4 @@
-// Reads sanky data
-//console.log(energyjson);
-//console.log("You done goof'd");
-var configSankeyEnergy = {
+var configSankeyEducation = {
     margin: { top: 10, left: 10, right: 10, bottom: 10 },
     nodes: {
       dynamicSizeFontNode: {
@@ -12,17 +9,18 @@ var configSankeyEnergy = {
       draggableX: false, // default [ false ]
       draggableY: true, // default [ true ]
       colors: d3.scaleOrdinal(d3.schemeCategory10)
-    },
+    }
+    ,
     links: {
-      formatValue: function(val) {
-        return d3.format(",.0f")(val) + ' TWh';
-      }
+      // formatValue: function(val) {
+      //   return d3.format(",.0f")(val) + ' TWh';
+      // }
     },
     tooltip: {
       infoDiv: true,
       labelSource: 'Input:',
       labelTarget: 'Output:'
     }
-}
+  }
 
-var objSankey = sk.createSankey('#chart1', configSankeyEnergy, energyjson);
+var objSankeyEd = sk.createSankey('#chart3', configSankeyEducation, educationjson);
